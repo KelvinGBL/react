b58b6c2aba0ae6501091eb4c8727653c19c6bfdf
Kelvin Gabriel <109625159+KelvinGBL@users.noreply.github.com>
2023-03-10 17:22:25 -0300

    Segundo Exercicio
    Eu instalei o pacote Material UI

--- exercicio1/src/App.js
+++ exercicio1/src/App.js
@@ -1,15 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import ImgMediaCard from './Item'
 
 function App() {
-  var name = "Kelvin Gabriel"
-  var curso = "Analise e Desenvolvimento de Sistemas"
-  var ano = 2022
   return (
     <div>
-      <p>Nome: {name}</p>
-      <p>Curso: {curso}</p>
-      <p>Ano: {ano}</p>
+      <ImgMediaCard/>
+      <ImgMediaCard/>
     </div>
   );
 }
---
+++ exercicio1/src/Item.jsx
@@ -0,0 +1,34 @@
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import './Item.css';
+
+export default function ImgMediaCard() {
+  return (
+    <Card sx={{ maxWidth: 345 }}>
+      <CardMedia
+        component="img"
+        alt="green iguana"
+        height="350"
+        image="https://images.unsplash.com/photo-1614027164847-1b28cfe1df60?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8MXx8fGVufDB8fHx8&w=1000&q=80"
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          Lion
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          Lizards are a widespread group of squamate reptiles, with over 6,000
+          species, ranging across all continents except Antarctica
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Share</Button>
+        <Button size="small">Learn More</Button>
+      </CardActions>
+    </Card>
+  );
+}
